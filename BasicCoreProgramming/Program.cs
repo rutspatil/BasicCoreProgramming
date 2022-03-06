f1e98144c876e1345e830a388e8546d3527fda02
rutuja patil <rutujapatil991@gmail.com>
2022-03-07 00:11:12 +0530

    Even Or Odd

--- BasicCoreProgramming/Program.cs
+++ BasicCoreProgramming/Program.cs
@@ -6,26 +6,22 @@ using System.Threading.Tasks;
 
 namespace BasicCoreProgramming
 {
-    class SwapNumber
+    class EvenOrOdd
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter Two Nubers :");
-            Console.Write("\nEnter Number 1 : ");
-            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the number : ");
+            //Convert user input into integer
+            int num = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Enter number 2 : ");
-            int num2 = Convert.ToInt32(Console.ReadLine());
-
-            //Swapping numbers
-            int temp = num1;
-            num1 = num2;
-            num2 = temp;
-
-            Console.WriteLine("\n AFTER SWAPPING NUMBER");
-            Console.WriteLine("\nFirst Number :" + num1);
-            Console.WriteLine("Second Number :" + num2);
-
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Number is Even");
+            }
+            else
+            {
+                Console.WriteLine("Number is Odd");
+            }
         }
     }
 }
