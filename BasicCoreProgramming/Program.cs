bb9f0e95a3038acd8c110e5dcf3ca72b824d4f42
rutuja patil <rutujapatil991@gmail.com>
2022-03-06 23:34:21 +0530

    Power Of Two

--- BasicCoreProgramming/Program.cs
+++ BasicCoreProgramming/Program.cs
@@ -6,22 +6,22 @@ using System.Threading.Tasks;
 
 namespace BasicCoreProgramming
 {
-     class LeapYear
+    class PowerOf2
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            int year;
-            Console.WriteLine("Enter the Year in Four Digits : ");
-            year = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the number : ");
+            int num = Convert.ToInt32(Console.ReadLine());
 
-            if (year % 4 == 0 && year % 100 == 0 || year % 400 == 0)
+            int i = 0;
+            int powerOfTwo = 1;
+            while (i <= num)
             {
-                Console.WriteLine("{0} is a Leap Year", year);
+                Console.WriteLine(i + " " + powerOfTwo);
+                powerOfTwo = 2 * powerOfTwo;
+                i = i + 1;
             }
-            else
-            {
-                Console.WriteLine("{0} is not a Leap Year", year);
-            }
+
         }
     }
 }
