20b6e93502924141e7ea7fedde4e6187824156bb
rutuja patil <rutujapatil991@gmail.com>
2022-03-06 23:23:05 +0530

    Leap Year

--- BasicCoreProgramming/Program.cs
+++ BasicCoreProgramming/Program.cs
@@ -6,44 +6,21 @@ using System.Threading.Tasks;
 
 namespace BasicCoreProgramming
 {
-    internal class Program
+     class LeapYear
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            int year;
+            Console.WriteLine("Enter the Year in Four Digits : ");
+            year = Convert.ToInt32(Console.ReadLine());
+
+            if (year % 4 == 0 && year % 100 == 0 || year % 400 == 0)
             {
-                //variable declaration
-                int result = 0;
-                int heads = 0;
-                int tails = 0;
-                double percentage1;
-                double percentage2;
-
-                Console.WriteLine("Enter number of timesflip a coin");
-                int num = Convert.ToInt32(Console.ReadLine()); //convert user input into integer
-                {
-
-                    for (int i = 0; i < num; i++)
-                    {
-                        Random random = new Random(); //Random function for flip coin
-                        result = random.Next(0, 2);
-                        if (result == 0)
-                        {
-                            heads++;
-                        }
-                        else
-                        {
-                            tails++;
-                        }
-
-
-                        Console.WriteLine("Tail was flipped {0} times", tails);
-                        percentage1 = ((float)heads / num) * 100;
-                        percentage2 = ((float)tails / num) * 100;
-                        Console.WriteLine("Percentage of Heads : " + percentage1);
-                        Console.WriteLine("Percentage of tails : " + percentage2);
-                    }
-
-                }
+                Console.WriteLine("{0} is a Leap Year", year);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a Leap Year", year);
             }
         }
     }
